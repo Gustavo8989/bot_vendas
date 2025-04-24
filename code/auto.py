4c60821369abfec9baefbdec9dcf7b6f658949ea
Gustavo <gustavohenriquealves418@gmail.com>
2025-04-24 20:52:52 -0300

    Começo webscraping

--- code/auto.py
+++ code/auto.py
@@ -1,9 +1,17 @@
 # Webscraping (mercadolivre) 
+from bs4 import BeautifulSoup 
+import pandas as pd 
+import numpy as np 
+import requests 
+import re 
+
 
 class Webscraping:
     def __init__(self):
         pass 
 
     def mercadolivre(self):
-        pass 
+        url_mercado = 'https://www.mercadolivre.com.br/' 
+        response = requests.get(url_mercado) 
+        soup = BeautifulSoup(response.text,'html.parser') 
 
