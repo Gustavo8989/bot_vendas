9c0dfe5bc690a8901f49aa7b6b009a4b7f5bf063
Gustavo <gustavohenriquealves418@gmail.com>
2025-05-24 21:08:41 -0300

    Bot

--- code/grap_bit.py
+++ code/grap_bit.py
@@ -1,30 +1,44 @@
 import matplotlib.pyplot as plt 
 import matplotlib.animation as animation
 import requests 
+import telebot 
 import time
+import os
 
-def price(api,price_applied:float,frame):
-    # Bitcoin 
-    price_bitcoin = requests.get(api)
-    data = price_bitcoin.json() 
-    price =float(data['price'])
-    # Real and Dolar 
-    dolar_em_real = "https://economia.awesomeapi.com.br/json/last/USD-BRL"
-    brl_response = requests.get(dolar_em_real) 
-    usd_to_brl = float(brl_response.json()['USDBRL']['bid'])
-    # Conversão 
-    bitcoin_to_real =  price * usd_to_brl 
-    my_money = price_applied 
-    qtd_brl_bit = float(my_money / bitcoin_to_real)
-    my_money_all = qtd_brl_bit * bitcoin_to_real
-    print(price_bitcoin)
+class telegram_bitcoin:
+    def price(self,api,price_applied:float,frame):
+        # Bitcoin 
+        self.price_bitcoin = requests.get(api)
+        self.data = price_bitcoin.json() 
+        self.price =float(data['price'])
+        # Real and Dolar 
+        self. dolar_em_real = "https://economia.awesomeapi.com.br/json/last/USD-BRL"
+        self.brl_response = requests.get(dolar_em_real) 
+        self.usd_to_brl = float(brl_response.json()['USDBRL']['bid'])
+        # Conversão 
+        self.bitcoin_to_real =  price * usd_to_brl 
+        self.my_money = price_applied 
+        self.qtd_brl_bit = float(my_money / bitcoin_to_real)
+        self.my_money_all = qtd_brl_bit * bitcoin_to_real
+
+        # Create graphic 
 
 
+    token = os.environ.get('auth.env')
+    bot = telebot.TeleBot(token) 
+    @bot.message_handler(commands=['price']) 
+    def send_welcome(self,message):
+        bot.reply_to(message,f"{bitcoin_to_real}")
 
+    @bot.message_handler(func=lambda msg:True)
+    def message_all(self,message):
+        bot.reply_to(message,"Comando negado, para ver o preço digite PRICE")
 
+bot.infinity_polling()
 if __name__ == "__main__":
+    price = telegram_bitcoin()
     while True:
         price("https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT",200,100)
         print('-'*40) 
-        time.sleep(3600) 
+        time.sleep(10) 
 
--- code/work_freela.py
+++ code/work_freela.py
@@ -1,4 +1,12 @@
+from bs4 import BeautifulSoup
+import pandas as pd
+import numpy as np
+import requests
+import re
+
 
 url_99 = "https://www.99freelas.com.br/projects?order=numero-de-interessados-menor&categoria=web-mobile-e-software"
-url_workona = "https://www.workana.com/jobs?category=it-programming&has_few_bids=1&language=xx"
+url_workona = "https://www.workana.com/jobs?category=it-programming&has_few_bids=1"
+
+
 
