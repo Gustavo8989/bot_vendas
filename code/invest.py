7a922b1e1e7741d7230a16b0a7878582381afb44
Gustavo <gustavohenriquealves418@gmail.com>
2025-05-31 17:18:12 -0300

    Atualizacao

--- code/invest.py
+++ code/invest.py
@@ -1,9 +1,19 @@
 import requests 
 import pandas as pd 
 import telebot
+import finnhub 
 import time 
 import os 
 
+def invest(api,money_invest,key_finnhub):
+    dolar = "https://economia.awesomeapi.com.br/json/last/USD-BRL"
+    dolar_response = requests.get(dolar) 
+    dolar_real = float(dolar_response.json()['USDBRL']['bid'])
+    alphavantage = requests.get(api) 
+    data_alphavantage = alphavantage.json()
+    data_finnhub = finnhub.Client(api_key=key_finnhub)
+    print(data_alphavantage) 
 
 
 
+invest('https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=demo',500,open("key_api_finnhub.txt","r"))
